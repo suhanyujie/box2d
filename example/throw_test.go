16d970ab8ebd57c219c45a3ca000353d0a4b3615
suhanyujie <suhanyujie@qq.com>
2023-04-03 22:56:10 +0800

    add some test code

--- example/throw_test.go
+++ example/throw_test.go
@@ -2,6 +2,7 @@ package example
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
 	"github.com/ByteArena/box2d"
@@ -32,7 +33,7 @@ func SetBody1(world *box2d.B2World) *box2d.B2Body {
 	return body
 }
 
-func SetBody2(world *box2d.B2World) *box2d.B2Body {
+func SetBody2InWorld(world *box2d.B2World) *box2d.B2Body {
 	bodyDef := box2d.NewB2BodyDef() // 刚体实例化
 	bodyDef.Type = box2d.B2BodyType.B2_dynamicBody
 	bodyDef.Position.Set(0., 4.)
@@ -42,20 +43,70 @@ func SetBody2(world *box2d.B2World) *box2d.B2Body {
 	dynBox.SetAsBox(1., 1.)
 
 	fixDef := box2d.NewB2Fixture()
+	// 密度
 	fixDef.SetDensity(1.0)
+	// 摩擦系数
 	fixDef.SetFriction(0.3)
+	// 恢复系数
 	fixDef.SetRestitution(1.0)
+	// 是否为传感器：当设置为 true 时，刚体发生碰撞的时候并不会发生碰撞响应（反弹），但是会接收到碰撞的信号，所以该属性可以理解为传感器。
+	fixDef.SetSensor(false)
 	fixDef.M_shape = dynBox
-
-	body.CreateFixture(fixDef.M_shape, fixDef.M_density) // 为 body 套上材质
+	// 为 body 套上材质
+	body.CreateFixture(fixDef.M_shape, fixDef.M_density)
+	body.SetLinearVelocity(box2d.B2Vec2{
+		X: 0,
+		Y: 1,
+	})
 
 	return body
 }
 
-func TestFrame1(t *testing.T) {
+func SetBody3InWorld(world *box2d.B2World) *box2d.B2Body {
+	bodyDef := box2d.NewB2BodyDef() // 刚体实例化
+	bodyDef.Type = box2d.B2BodyType.B2_dynamicBody
+	bodyDef.Position.Set(0., 0.)
+	body := world.CreateBody(bodyDef)
+
+	dynBox := box2d.NewB2PolygonShape()
+	dynBox.SetAsBox(2., 2.)
+
+	fixDef := box2d.NewB2Fixture()
+	// 密度
+	fixDef.SetDensity(1.0)
+	// 摩擦系数
+	fixDef.SetFriction(0.3)
+	// 恢复系数
+	fixDef.SetRestitution(1.0)
+	// 是否为传感器：当设置为 true 时，刚体发生碰撞的时候并不会发生碰撞响应（反弹），但是会接收到碰撞的信号，所以该属性可以理解为传感器。
+	fixDef.SetSensor(false)
+	fixDef.M_shape = dynBox
+	// 设置位置 cpp_compliance_test.go:166
+
+	body.CreateFixture(fixDef.M_shape, fixDef.M_density) // 为 body 套上材质
+
+	body.SetLinearVelocity(box2d.B2Vec2{
+		X: 0,
+		Y: 20,
+	})
+
+	return body
+}
+
+// todo
+func SetGround(world *box2d.B2World) {
+	bodyDef := box2d.NewB2BodyDef()
+	bodyDef.Position.Set(0., 0.)
+
+	world.CreateBody(bodyDef)
+}
+
+// world 的 hello world 示例。物体的运动
+func TestHelloWorld1(t *testing.T) {
 	var timeStep float64 = 1. / 60.
 	world := GetWorld()
-	body1 := SetBody2(world)
+	SetBody2InWorld(world)
+	SetBody3InWorld(world)
 	// last := time.Now().UnixMilli()
 	var animate func()
 	animate = func() {
@@ -69,16 +120,55 @@ func TestFrame1(t *testing.T) {
 		world.Step(timeStep, velocityIterations, positionIterations)
 		world.ClearForces()
 	}
-	for i := 0; i < 60; i++ {
+	for i := 0; i < 1000; i++ {
 		animate()
 
-		posi := body1.GetPosition()
-		angle := body1.GetAngle()
-		fmt.Printf("res posi x: %v, y:%v, angle:%v \n", posi.X, posi.Y, angle)
+		for cnArr := world.GetContactList(); cnArr != nil; cnArr.GetNext() {
+			bodyA := cnArr.GetFixtureA()
+			bodyB := cnArr.GetFixtureB()
+			ta := bodyA.GetBody().GetTransform()
+			tb := bodyB.GetBody().GetTransform()
+			touching := box2d.B2TestOverlapShapes(bodyA.M_shape, cnArr.GetChildIndexA(), bodyB.M_shape, cnArr.GetChildIndexB(), ta, tb)
+			if touching {
+				posi := bodyA.GetBody().GetPosition()
+				angle := bodyA.GetBody().GetAngle()
+				fmt.Printf("res posi x: %v, y:%v, angle:%v \n", posi.X, posi.Y, angle)
+			}
+			continue
+
+			posi := bodyA.GetBody().GetPosition()
+			angle := bodyA.GetBody().GetAngle()
+			fmt.Printf("res posi x: %v, y:%v, angle:%v \n", posi.X, posi.Y, angle)
+			if bodyA.IsSensor() {
+				world.DestroyBody(bodyA.M_body)
+				log.Printf("destroy a")
+			}
+
+			posi = bodyB.GetBody().GetPosition()
+			angle = bodyB.GetBody().GetAngle()
+			fmt.Printf("res posi x: %v, y:%v, angle:%v \n", posi.X, posi.Y, angle)
+			if bodyB.IsSensor() {
+				world.DestroyBody(bodyB.M_body)
+				log.Printf("destroy b")
+			}
+		}
 	}
 }
 
-func NewDynBody1() {
-	//bodyDef := box2d.MakeB2BodyDef()
-	//bodyDef.Type = box2d.MakeB2DynamicTree()
+// ref https://www.jianshu.com/p/7681431618ec
+func NewDefShape1() {
+	// 三角形
+	// 描述三角形的三个点的坐标
+	vertiArr := make([]box2d.B2Vec2, 3)
+	vertiArr[0].Set(0., 0.)
+	vertiArr[1].Set(1., 0.)
+	vertiArr[2].Set(0., 1.)
+	cnt := 3
+	polygon := box2d.MakeB2PolygonShape()
+	polygon.Set(vertiArr, cnt)
+
+	// edge shape
+	// 为了帮助为游戏创建自由形式的静态环境。边缘形状没有体积
+	edge1 := box2d.MakeB2EdgeShape()
+	edge1.Set(box2d.B2Vec2{0., 0.}, box2d.B2Vec2{0., 0.})
 }
